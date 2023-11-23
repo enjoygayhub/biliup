50aa1014c3306cadcb503187cd8c59196bd5c717
杜松 <dusong@mail.jj.cn>
2023-11-23 21:01:54 +0800

    zuncun

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 from biliup.downloader import download
 from biliup.plugins import logger
-from biliup.plugins.huya import huya
+
 
 if __name__ == '__main__':
 
     logger.info('This is a log info')
     
-    pg =  huya('test', 'https://www.huya.com/21372', suffix='flv')
-    pg.start()
+    download('test', 'https://www.huya.com/21372', suffix='flv')
+    
